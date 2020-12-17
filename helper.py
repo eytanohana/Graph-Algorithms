85e55d964ccc5b9c9a661ec58a4d734dc3972a17
Osher Boudara <osherboudara99@gmail.com>
2020-12-17 14:09:28 -0800

    list/dict comprehensions

--- helper.py
+++ helper.py
@@ -4,36 +4,28 @@ import matplotlib.pyplot as plt
 
 def draw_graph(G):
     
-    marked_nodes = []
-    unmarked_nodes = []
-    for node, attrs in G.nodes.items():
-        print(node, attrs)
-        if attrs.get('marked', False) == True:
-            marked_nodes.append(node)
-        else:
-            unmarked_nodes.append(node)
-            
-    print(marked_nodes)
+    marked_nodes = [node for node, attrs in G.nodes.items() if attrs.get('marked', False) == True]
+    unmarked_nodes = list(set(G.nodes) - set(marked_nodes))
             
     marked_edges = [e for e, attrs in G.edges.items() if attrs.get('marked', False) == True]    
-    unmarked_edges = [e for e, attrs in G.edges.items() if not attrs.get('marked', False) == True]
+    unmarked_edges = list(set(G.edges) - set(marked_edges))
     
     pos = nx.spring_layout(G) # positions for all nodes
 
     nx.draw_networkx_nodes(G, pos,
                        nodelist=marked_nodes,
-                       node_color='g',
+                       node_color='#FF00FF',
                        node_size=500)
     
     nx.draw_networkx_nodes(G, pos,
                        nodelist=unmarked_nodes,
-                       node_color='b',
+                       node_color='#7EFF67',
                        node_size=500)
 
     # edges
     nx.draw_networkx_edges(G, pos,
                            edgelist=marked_edges,
-                           edge_color='g',
+                           edge_color='#FF00FF',
                            width=2)
     
     nx.draw_networkx_edges(G, pos,
