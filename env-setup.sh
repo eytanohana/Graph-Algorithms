02a750a1b8f111dcec500f3c4cc4c6eafb38c709
eytanohana <eohana98@gmail.com>
2023-07-14 16:28:09 +0300

    rm env-setup script

--- env-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# requires python 3.11 to be installed
-echo "creating python3.11 env in .venv directory"
-python3.11 -m venv .venv
-source .venv/bin/activate
-echo "Installing requirements..."
-pip install -r requirements.txt
